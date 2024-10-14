32bd7f51a4876190d669f51c691e31b5c94c71a1
ifat <i>
2024-10-14 19:55:47 +0100

    problem 3 solved

---
+++ Problem_3.java
@@ -0,0 +1,11 @@
+public class Problem_3 {
+    class CustomerLister {
+        public static void main(String[] args) {
+            String[] customerName = { "Cathy", "Ben", "Jorge", "Wanda", "Freddie" };
+
+         for(int i = 0;  i < customerName.length; i++){
+            System.err.println(customerName[i]);
+         }
+        }
+    }
+}
